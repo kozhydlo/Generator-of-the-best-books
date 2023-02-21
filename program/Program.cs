e080d13ef11466ece355666eb27fd285a8e8881c
Mark Kozhydlo <99179725+kozhydlomark2010@users.noreply.github.com>
2023-02-21 15:47:25 +0200

    Add files via upload

---
+++ program/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Найкращі книги:");
+
+        // Створення масиву книг
+        string[] books = new string[]
+        {
+            "Війна і мир",
+            "1984",
+            "Гаррі Поттер і філософський камінь",
+            "Улисс",
+            "Майстер і Маргарита",
+            "Таємниця старого замку",
+            "Сто років самотності",
+            "Вітер у вербах",
+            "Мертві душі",
+            "Три товариші"
+        };
+
+        // Виведення списку книг на екран
+        foreach (string book in books)
+        {
+            Console.WriteLine("- " + book);
+        }
+
+        Console.ReadLine();
+    }
+}
